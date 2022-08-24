fb585988e6e056772dd2b8d2ea2065d154d92e87
bobroej <tverdune@yandex.ru>
2022-08-24 09:49:54 +0300

    allure notifications added pyproject.toml added

--- log_functions.py
+++ log_functions.py
@@ -1,5 +1,7 @@
 import re
 import types
+import allure
+
 from functools import wraps
 
 
@@ -8,13 +10,23 @@ def humanify(name: str):
 
 
 def step(fn):
+    @allure.step(humanify(fn.__name__))
     @wraps(fn)
     def fn_with_logging(*args, **kwargs):
+        """
+        заменил, потому что при добавлении аргумента в функцию given_sign_up_form_opened, а не в метод, вываливается
+        ошибка AttributeError
+
         is_method = (
                 args
                 and isinstance(args[0], object)
                 and isinstance(getattr(args[0], fn.__name__), types.MethodType)
         )
+        """
+        try:
+            is_method = isinstance(getattr(args[0], fn.__name__), types.MethodType)
+        except:
+            is_method = False
 
         args_to_log = args[1:] if is_method else args
         args_and_kwargs_to_log_as_strings = [
@@ -22,10 +34,16 @@ def step(fn):
             *[f'{key} = {value}' for key, value in kwargs.items()]
         ]
         args_and_kwargs_string = (
-            ': ' + ', '.join(map(str, args_and_kwargs_to_log_as_strings))
-              if args_and_kwargs_to_log_as_strings
-              else ''
+            ', '.join(map(str, args_and_kwargs_to_log_as_strings))
+            if args_and_kwargs_to_log_as_strings
+            else ''
         )
+        if is_method:
+            with allure.step(f'Method of {args[0].__class__.__name__}'):
+                pass
+
+        if args_and_kwargs_string:
+            allure.attach(f'{args_and_kwargs_string}', 'Human-readable args and kwargs', allure.attachment_type.TEXT)
 
         print(
             (f'[{args[0].__class__.__name__}] ' if is_method else '')
@@ -36,12 +54,6 @@ def step(fn):
         return fn(*args, **kwargs)
 
     return fn_with_logging
-
-
-@step
-def given_sign_up_form_opened():
-    # print(given_sign_up_form_opened.__name__)
-    ...
 
 
 class SignUpForm:
@@ -73,14 +85,22 @@ class DashBoard:
         ...
 
 
+@step
+def given_sign_up_form_opened(arg):
+    print(arg)
+    ...
+
+
 sign_up_form = SignUpForm()
 dashboard = DashBoard()
 
-given_sign_up_form_opened()
-(sign_up_form
- .fill_name('zhenya', surname='tverdun')
- .fill_email(value='tverdune@ya.ru')
- .fill_password('qwerty')
- .submit()
- )
-dashboard.go_to_profile()
+
+def test_allure_notifications():
+    given_sign_up_form_opened('test')
+    (sign_up_form
+     .fill_name('zhenya', surname='tverdun')
+     .fill_email('tverdune@ya.ru')
+     .fill_password('qwerty')
+     .submit()
+     )
+    dashboard.go_to_profile()
